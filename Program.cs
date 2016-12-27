29a3667132babe4656eb973d76943d514013c376
pontresina <frank.wahler@maksimo.de>
2016-12-27 23:44:57 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// 23.12.2016 fwa created
+// 27.12.2016 fwa Test git clone
 // https://weblog.west-wind.com/posts/2016/Jun/29/First-Steps-Exploring-NET-Core-and-ASPNET-Core#Wherearewe?
 
 // vs code
